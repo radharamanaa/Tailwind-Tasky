9f332a635609180009e74641323309f6aefd03c2
radharamanaa <colouredpages@gmail.com>
2022-03-18 17:31:43 +0530

    Tailwin-Good_Tasky-design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        "press-start": ['"Press Start 2P"', "Inter"],
+      },
+    },
+  },
+};
